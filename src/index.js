0893a2141786e7794b611a955251fec11abff64d
Akshar Saxena <aksharsaxena2003@gmail.com>
2024-02-04 13:53:19 +0530

    added cors

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { UploadHandler } from "./handler/UploadHandler.js";
 const app = express();
 app.use(
     cors({
-        origin: "*",
+        origin: "https://symbiosis-swap-shop.vercel.app",
     })
 );
 app.use(express.json());
